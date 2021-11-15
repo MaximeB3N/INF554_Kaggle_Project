4d3e0fda5a0b4051f8fc9d8351a2ae793f6ef620
Erwan Umlil <erwan.umlil@polytechnique.edu>
2021-11-15 18:13:13 +0100

    baseline: change data paths

--- src/baseline.py
+++ src/baseline.py
@@ -6,17 +6,17 @@ from sklearn.linear_model import Lasso
 
 
 # read training data
-df_train = pd.read_csv('train.csv', dtype={'author': np.int64, 'hindex': np.float32})
+df_train = pd.read_csv('../data/train.csv', dtype={'author': np.int64, 'hindex': np.float32})
 n_train = df_train.shape[0]
 
 # read test data
-df_test = pd.read_csv('test.csv', dtype={'author': np.int64})
+df_test = pd.read_csv('../data/test.csv', dtype={'author': np.int64})
 n_test = df_test.shape[0]
 
-# load the graph    
-G = nx.read_edgelist('coauthorship.edgelist', delimiter=' ', nodetype=int)
+# load the graph
+G = nx.read_edgelist('../data/coauthorship.edgelist', delimiter=' ', nodetype=int)
 n_nodes = G.number_of_nodes()
-n_edges = G.number_of_edges() 
+n_edges = G.number_of_edges()
 print('Number of nodes:', n_nodes)
 print('Number of edges:', n_edges)
 
@@ -25,7 +25,7 @@ print('Number of edges:', n_edges)
 core_number = nx.core_number(G)
 
 # create the training matrix. each node is represented as a vector of 3 features:
-# (1) its degree, (2) its core number 
+# (1) its degree, (2) its core number
 X_train = np.zeros((n_train, 2))
 y_train = np.zeros(n_train)
 for i,row in df_train.iterrows():
@@ -41,7 +41,7 @@ for i,row in df_test.iterrows():
     node = row['author']
     X_test[i,0] = G.degree(node)
     X_test[i,1] = core_number[node]
-    
+
 # train a regression model and make predictions
 reg = Lasso(alpha=0.1)
 reg.fit(X_train, y_train)
@@ -52,8 +52,3 @@ df_test['hindex'] = pd.Series(np.round_(y_pred, decimals=3))
 
 
 df_test.loc[:,["author","hindex"]].to_csv('submission.csv', index=False)
-
-
-
-
-
